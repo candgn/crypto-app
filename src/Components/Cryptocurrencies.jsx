f51af16d5aebc320eab077a4dd5e37614fbf7154
Can Dogan <can.dogan@ambeent.ai>
2022-01-07 19:10:24 +0300

    search bar added to cryptocurrencies

--- src/Components/Cryptocurrencies.jsx
+++ src/Components/Cryptocurrencies.jsx
@@ -1,21 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import millify from "millify";
 import { Link } from "react-router-dom";
 import { Card, Row, Col, Input } from "antd";
 
 import { useGetCryptosQuery } from "../Services/cryptoApi";
 
-const Cryptocurrencies = ({simplified}) => {
-  const count = simplified ? 10 : 100
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   console.log(cryptos);
+
+  useEffect(() => {
+    const filteredData = cryptosList?.data?.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+    );
+
+    setCryptos(filteredData);
+  }, [cryptosList, searchTerm]);
 
   if (isFetching) return "Loading.....";
 
   return (
     <>
+      {!simplified && (
+        <div className="search-crypto">
+          <Input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
+
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency) => {
           return (
