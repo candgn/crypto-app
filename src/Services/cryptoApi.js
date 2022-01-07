37dde28d01660c90ce4c5c6b1450cf25b81d2a7a
Can Dogan <can.dogan@ambeent.ai>
2022-01-07 17:47:25 +0300

    homepage elements added - coinranking rapidapi connected - cryptocurrencies currencies added

---
+++ src/Services/cryptoApi.js
@@ -0,0 +1,44 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "ecca664c82mshdc01dfda8e381edp12c9c1jsnf0dad8674ca7",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com/";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
+
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/coins',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       timePeriod: '24h',
+//       symbols: 'BTC',
+//       uuids: 'Qwsogvtv82FCd',
+//       tiers: '1',
+//       tags: 'defi',
+//       orderBy: 'marketCap',
+//       search: 'Bitco',
+//       orderDirection: 'desc',
+//       limit: '50',
+//       offset: '0'
+//     },
+//     headers: {
+//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//       'x-rapidapi-key': 'ecca664c82mshdc01dfda8e381edp12c9c1jsnf0dad8674ca7'
+//     }
+//   };
